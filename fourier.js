3ef69ef4ebd35d74c5ea53fdd5333d269b6a60da
luke-kaufman <lukekaufman48@gmail.com>
2020-08-03 22:21:14 -0500

    v 1.0 working square wave

--- fourier.js
+++ fourier.js
@@ -1,4 +1,6 @@
 let angle = 0;
+let signal = [];
+let epicycleEdge = 0;
 
 function setup() {
     createCanvas(1000, 600);
@@ -6,16 +8,64 @@ function setup() {
   
 function draw() {
     background(0);
-    
-    //main circle
-    let radius = 200;
+    translate(width/4,height/2);
+    let numOfCircles=50;
+    let alpha = 130;
+    let x=0;
+    let y=0;
+    for(let i=0; i<numOfCircles; i++){
+      previousx = x;
+      previousy = y;
+      
+      //code that determines signal
+      let n = 2*i + 1; // n is odd numbers starting from 1
+      let radius = 100 * (4/(n*PI))
+      x += radius * cos(n*angle);
+      y += radius * sin(n*angle); 
+
+      //finds full extent of epicycles for signal placement
+      let currentEdge = previousx + radius;
+      if(currentEdge>epicycleEdge){
+        epicycleEdge=currentEdge;
+      }
+
+      //circles
+      noFill();
+      stroke(255,alpha);
+      ellipse(previousx, previousy, radius*2);
+      
+      //point
+      fill(255,alpha);
+      ellipse(x,y,10);   
+      
+      //line to points
+      stroke(255);
+      line(previousx, previousy, x, y);
+      
+    }
+    signal.unshift(y); //take y value for drawing and add to beginning
+
+    //line to signal
+    translate(epicycleEdge, 0);
+    stroke(255, alpha);
+    line(x-epicycleEdge, y, 0, signal[0])
+
+    //signal
+    beginShape();
     noFill();
     stroke(255);
-    ellipse(width/4, height/2, radius*2);
+    for(let i = 0; i<signal.length; i++){
+      vertex(i,signal[i]);
+    }
+    endShape();
     
-    //point
-    let x = radius * cos(angle);
-    let y = radius * sin(angle); 
-
-    angle += 0.1;
+    //update angle
+    angle += 0.01;
+    
+    if(angle>TWO_PI){
+      angle = 0;
+    }
+    if(signal.length>600){
+      signal.pop();
+    }
   }
