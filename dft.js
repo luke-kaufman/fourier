b2ed8cbb41693df58b7a98fc93d968f8d6c80705
luke-kaufman <lukekaufman48@gmail.com>
2020-10-05 11:05:08 -0500

    Sawtooth and squar with sliders

---
+++ dft.js
@@ -0,0 +1,17 @@
+// discrete fourier transform - 
+// takes input signal as an array and computes the discrete fourier transform
+
+function DiscreteFourierTransform(values){
+    let X = [];
+    
+    for(let k=0; k<N; k++){
+        let real = 0;
+        let imaginary = 0;
+        for(let n=0; n<N; n++){
+            real += ;
+            imaginary += ;
+        }
+    }
+
+    return null; 
+}
--- fourier.js
+++ fourier.js
@@ -1,48 +1,88 @@
 let angle = 0;
 let signal = [];
-let epicycleEdge = 0;
+let epicycleEdge = 0; //really need to find this before animation starts
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+let canvasWidth = 1000;
+let canvasHeight = 600;
+let angVelocity = 0.02;
+let alpha = 130; 
+//Flags
+let SQUARE_WAVE = 1; //radio value
+let SAWTOOTH_WAVE = 2; // radio value
+//Componenets
+let NSlider;
+let previousNSliderValue;
+let angVelocitySlider;
+let previousAngVelocitySliderValue;
+let wavesRadio;
+let values;
+let fourierY;
+
 
 function setup() {
     createCanvas(1000, 600);
+    //NSlider
+    NSlider = createSlider(1,50,7);
+    //angVelocitySlider
+    angVelocitySlider = createSlider(5,100,30);
+    //Radio
+    wavesRadio = createRadio();
+    wavesRadio.option(SQUARE_WAVE, 'Square Wave');
+    wavesRadio.option(SAWTOOTH_WAVE, 'Sawtooth Wave');
+    wavesRadio.selected(2);
+    //triangleWaveCheckbox = createCheckbox('Triangle Wave', false);
+    //y = [12,57,23,-23,-75,86,-34,-68,56,32,3,46,-68,12,57,23,-23,-75,86,-34,-68,56,32,3,46,-68];
+    // dft(y);
   }
   
 function draw() {
     background(0);
     translate(width/4,height/2);
-    let numOfCircles=50;
-    let alpha = 130;
     let x=0;
     let y=0;
-    for(let i=0; i<numOfCircles; i++){
+    let radius=0;
+    let v = null;
+    
+
+    for(let i=0; i<(NSlider.value()); i++){
       previousx = x;
       previousy = y;
-      
-      //code that determines signal
-      let n = 2*i + 1; // n is odd numbers starting from 1
-      let radius = 100 * (4/(n*PI))
-      x += radius * cos(n*angle);
-      y += radius * sin(n*angle); 
-
-      //finds full extent of epicycles for signal placement
-      let currentEdge = previousx + radius;
-      if(currentEdge>epicycleEdge){
-        epicycleEdge=currentEdge;
+  
+      currentWave = wavesRadio.value();
+      //make into switch case
+      if(currentWave == 1){
+        console.log("case 1 begin");
+        v = squareWave(x,y,i);
+        x = v[0];
+        y = v[1];
+        radius = v[2];
+        console.log("case 1 end"); 
       }
-
+      if(currentWave == 2){
+        v = sawtoothWave(x,y,i);
+        x = v[0];
+        y = v[1];
+        radius = v[2];
+        console.log("case 2");
+      }
+    
+    
       //circles
       noFill();
       stroke(255,alpha);
       ellipse(previousx, previousy, radius*2);
-      
+          
       //point
       fill(255,alpha);
       ellipse(x,y,10);   
-      
+          
       //line to points
       stroke(255);
       line(previousx, previousy, x, y);
-      
+          
     }
+
     signal.unshift(y); //take y value for drawing and add to beginning
 
     //line to signal
@@ -60,12 +100,35 @@ function draw() {
     endShape();
     
     //update angle
-    angle += 0.01;
+    angle += angVelocitySlider.value()/1000;
     
     if(angle>TWO_PI){
       angle = 0;
     }
-    if(signal.length>600){
+
+    if(signal.length>(screenWidth-epicycleEdge)){
       signal.pop();
     }
-  }
+
+}
+
+function sawtoothWave(x,y,i) {
+    //sawtooth wave
+    let n = (i+1) * -1; 
+    let radius = 200 * (1/(n*PI))
+    x += radius * cos(n*angle);
+    y += radius * sin(n*angle);
+
+    return [x,y,radius];
+}
+
+
+function squareWave(x,y,i) {
+
+  let n = (2*i + 1) * -1; // n is odd numbers starting from 1
+  let radius = 100 * (4/(n*PI))
+  x += radius * cos(n*angle);
+  y += radius * sin(n*angle); 
+
+  return [x,y,radius];
+}
